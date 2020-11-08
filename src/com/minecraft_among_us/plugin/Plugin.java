374211654b7a2181cec4e053da2d31c656ac223a
Lelberto <jsurieux@gmail.com>
2020-11-08 14:25:09 +0100

    Rename Main to Plugin Add essentials plugin methods

---
+++ src/com/minecraft_among_us/plugin/Plugin.java
@@ -0,0 +1,36 @@
+package com.minecraft_among_us.plugin;
+
+import com.minecraft_among_us.plugin.config.ConfigurationManager;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin {
+
+    public static org.bukkit.plugin.Plugin getPlugin() {
+        return Bukkit.getPluginManager().getPlugin(Plugin.getPluginName());
+    }
+
+    public static void log(String msg) {
+        System.out.println("[" + Plugin.getPluginName() + "] " + msg);
+    }
+
+    public static String getPluginName() {
+        return "MinecraftAmongUs";
+    }
+
+    public static World getDefaultWorld() {
+        return Bukkit.getWorlds().get(0);
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+        ConfigurationManager.getInstance().save();
+    }
+}
