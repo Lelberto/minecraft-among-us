afeca08fc2d3368ee23fd778e341d658e9a9b7af
Lelberto <jsurieux@gmail.com>
2020-11-09 21:21:30 +0100

    Add task listeners registration Add test command

--- src/com/minecraft_among_us/plugin/Plugin.java
+++ src/com/minecraft_among_us/plugin/Plugin.java
@@ -1,10 +1,13 @@
 package com.minecraft_among_us.plugin;
 
+import com.minecraft_among_us.plugin.commands.TestCommand;
 import com.minecraft_among_us.plugin.game.Game;
 import com.minecraft_among_us.plugin.inventories.ColorInventory;
 import com.minecraft_among_us.plugin.inventories.ComputerInventory;
 import com.minecraft_among_us.plugin.inventories.GameSettingsInventory;
 import com.minecraft_among_us.plugin.inventories.HatInventory;
+import com.minecraft_among_us.plugin.tasks.SimonTask;
+import com.minecraft_among_us.plugin.tasks.TemperatureTask;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -34,12 +37,23 @@ public class Plugin extends JavaPlugin {
     @Override
     public void onEnable() {
         super.onEnable();
+
+        // Commands registration
+        this.getCommand("test").setExecutor(new TestCommand());
+
         // Listeners registration
         Bukkit.getPluginManager().registerEvents(new Game.Listener(), this);
         Bukkit.getPluginManager().registerEvents(new ComputerInventory.Listener(), this);
         Bukkit.getPluginManager().registerEvents(new HatInventory.Listener(), this);
         Bukkit.getPluginManager().registerEvents(new GameSettingsInventory.Listener(), this);
         Bukkit.getPluginManager().registerEvents(new ColorInventory.Listener(), this);
+        Bukkit.getPluginManager().registerEvents(new SimonTask.Listener(), this);
+        Bukkit.getPluginManager().registerEvents(new TemperatureTask.Listener(), this);
+
+        // TODO Used for debug only, remove it in production
+        // Add online players in game to avoid reconnect
+        Game game = Game.getInstance();
+        Bukkit.getOnlinePlayers().forEach(player -> game.getPlayers().add(new AmongUsPlayer(player.getUniqueId(), game.randomColor())));
     }
 
     @Override
