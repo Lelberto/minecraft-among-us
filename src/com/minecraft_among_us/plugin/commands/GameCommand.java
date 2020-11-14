a48cd0c1b43a42be4cac06b9d2d3e3e10b63ee46
Lelberto <jsurieux@gmail.com>
2020-11-14 20:00:23 +0100

    Add starting game on game command

--- src/com/minecraft_among_us/plugin/commands/GameCommand.java
+++ src/com/minecraft_among_us/plugin/commands/GameCommand.java
@@ -2,6 +2,7 @@ package com.minecraft_among_us.plugin.commands;
 
 import com.minecraft_among_us.plugin.Plugin;
 import com.minecraft_among_us.plugin.game.Game;
+import com.minecraft_among_us.plugin.game.GameState;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -17,18 +18,26 @@ public class GameCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (sender instanceof ConsoleCommandSender || sender.isOp()) {
+            Game game = Game.getInstance();
             if (args.length >= 1) {
+                if (args[0].equalsIgnoreCase("start")) {
+                    if (game.getState().equals(GameState.HUB)) {
+                        game.start();
+                    } else {
+                        sender.sendMessage(Plugin.getPluginNameChat() + "§cGame is already started");
+                    }
+                }
                 if (args[0].equalsIgnoreCase("devmode")) {
                     if (args.length == 1) {
                         sender.sendMessage(Plugin.getPluginNameChat() + "Dev mode is " + Game.getInstance().isDevMode());
                         return true;
                     }
                     if (args[1].equalsIgnoreCase("on")) {
-                        Game.getInstance().setDevMode(true);
+                        game.setDevMode(true);
                         sender.sendMessage(Plugin.getPluginNameChat() + "Dev mode is now on");
                         return true;
                     } else if (args[1].equalsIgnoreCase("off")) {
-                        Game.getInstance().setDevMode(false);
+                        game.setDevMode(false);
                         sender.sendMessage(Plugin.getPluginNameChat() + "Dev mode is now off");
                         return true;
                     }
